3ead466eeef38c871c8617b8a20a5a6fe378f2ee
mridulrb <Mridul.Bhandari@ibm.com>
2021-11-01 10:35:09 +0400

    Update neuralNetwork.R

--- Digit_Recognizer/neuralNetwork.R
+++ Digit_Recognizer/neuralNetwork.R
@@ -15,25 +15,25 @@ nrow(trainDataset)   # Show number of records
 names(trainDataset)  # Show fields names or columns names
 
 #--------------------------------------------------------------
-# Step 5: Choose Target variable
+# Step 1: Choose Target variable
 #--------------------------------------------------------------
-cat("\nStep 5: Choose Target Variable")
+cat("\nStep 1: Choose Target Variable")
 target  <- names(trainDataset)[1] #label name
 target
 
 
 
 #--------------------------------------------------------------
-# Step 6: Choose inputs Variables
+# Step 2: Choose inputs Variables
 #--------------------------------------------------------------
-cat("\nStep 6: Choose Inputs Variable")
+cat("\nStep 2: Choose Inputs Variable")
 inputs <- setdiff(names(trainDataset),target)
 inputs
 length(inputs)
 #--------------------------------------------------------------
-# Step 7: Select Training Data Set
+# Step 3: Select Training Data Set
 #--------------------------------------------------------------
-cat("\nStep 7: Select training dataset")
+cat("\nStep 3: Select training dataset")
 
 head(trainDataset)    # Show Top 6 records
 nrow(trainDataset)    # Show number of train Dataset
@@ -49,9 +49,9 @@ Xfinal <- as.matrix(Xreduced) %*% pcaX$rotation[,1:45]
 Y <- class.ind(Y)
 
 #--------------------------------------------------------------
-# Step 8: Select Testing Data Set
+# Step 4: Select Testing Data Set
 #--------------------------------------------------------------
-cat("\nStep 8: Select testing dataset")
+cat("\nStep 4: Select testing dataset")
 testDataset <- read.csv("mnist_test.csv")
 head(testDataset)
 nrow(testDataset)
@@ -61,9 +61,9 @@ testreduced <- test/255
 testreduced <- as.matrix(testreduced) %*% pcaX$rotation[,1:45]
 #write.csv(testDataset,file="testDataset.csv",row.names=FALSE);
 #--------------------------------------------------------------
-# Step 9: Model Building (Training)
+# Step 5: Model Building (Training)
 #--------------------------------------------------------------
-cat("\nStep 9: Model Building -> ", modelName)
+cat("\nStep 5: Model Building -> ", modelName)
 #formula <- as.formula(paste(target, "~", paste(c(inputs), collapse = "+")))
 #formula
 
@@ -72,61 +72,61 @@ model <- nnet(Xfinal,Y,size=150,softmax=TRUE,maxit=130,MaxNWts =80000)
 model
 
 #--------------------------------------------------------------
-# Step 10: Prediction (Testing)
+# Step 6: Prediction (Testing)
 #--------------------------------------------------------------
-cat("\nStep 10: Prediction using -> ", modelName)
+cat("\nStep 6: Prediction using -> ", modelName)
 Predicted <- predict(model, testreduced,type="class")
 Predicted <- as.data.frame(Predicted);
 head(Predicted) 
 
 
 #--------------------------------------------------------------
-# Step 11: Extracting Actual
+# Step 7: Extracting Actual
 #--------------------------------------------------------------
-cat("\nStep 11: Extracting Actual")
+cat("\nStep 7: Extracting Actual")
 Actual <- as.double(unlist(testDataset[target]))
 head(Actual)
 
 
 
 #--------------------------------------------------------------
-# Step 12: Model Evaluation
+# Step 8: Model Evaluation
 #--------------------------------------------------------------
-cat("\nStep 12: Model Evaluation")
+cat("\nStep 8: Model Evaluation")
 
-# Step 12.1: Accuracy
+# Step 8.1: Accuracy
 accuracy <- round(mean(Actual==Predicted) *100,2)
 accuracy
 
 
-# Step 12.2: Total Time
+# Step 8.2: Total Time
 totalTime = proc.time()[3] - startTime
 totalTime
 
 
-# Step 12.3: Save evaluation resut 
+# Step 8.3: Save evaluation resut 
 result <- data.frame(modelName,accuracy, totalTime)[1:1,]
 result
 
 
 
 #--------------------------------------------------------------
-# Step 13: Writing to file
+# Step 9: Writing to file
 #--------------------------------------------------------------
-cat("\nStep 13: Writing to file")
+cat("\nStep 9: Writing to file")
 
-# Step 13.1: Writing to file (evaluation result)
+# Step 9.1: Writing to file (evaluation result)
 write.csv(result, file=paste(modelName,"-Evaluation-Result.csv",sep=''), row.names=FALSE)
 
-# Step 13.2: Writing to file (Actual and Predicted)
+# Step 9.2: Writing to file (Actual and Predicted)
 write.csv(data.frame(Actual,Predicted), file=paste(modelName,"-ActualPredicted-Result.csv",sep=''), row.names=FALSE)
 
 
 
 #--------------------------------------------------------------
-# Step 14: Saving the Model
+# Step 10: Saving the Model
 #--------------------------------------------------------------
-cat("\nStep 14: Saving the Model ->",modelName)
+cat("\nStep 10: Saving the Model ->",modelName)
 save.image(file=paste(modelName,"-Model.RData",sep=''))
 
 
@@ -137,5 +137,3 @@ cat("\nTotal Time Taken: ", totalTime," sec")
 #--------------------------------------------------------------
 #                           END 
 #--------------------------------------------------------------
-
-
